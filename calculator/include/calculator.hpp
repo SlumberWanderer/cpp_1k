58f27dc542c2f8a5732a7433c7a55fe7b6a024af
Maksim <Win11Maksim>
2026-02-10 18:14:31 +0200

    lab3.1

--- calculator/include/calculator.hpp
+++ calculator/include/calculator.hpp
@@ -1,17 +1,22 @@
 #pragma once
 #include <cmath>
 
+using namespace std;
+
 class Calculator {
 	const double a_;
 	const double b_;
 public:
-	Calculator(const double& a, const double& b): a_(a), b_(b) {}
-	
+	Calculator(const double& a, const double& b) : a_(a), b_(b) {}
+
 	const double calculate(const double& x) const
 	{
-		return (std::sin(b_ * x) / x) * std::exp(-a_ * x);
+		double sin_part = sin(b_ * x) / x;
+		double exp_part = exp(-a_ * x);
+
+		return sin_part * exp_part;
 	}
-	
+
 	const double operator()(const double& x) const
 	{
 		return this->calculate(x);
--- calculator/src/main.cpp
+++ calculator/src/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include "calculator.hpp"
 
-using std::cout;
-using std::cin;
-using std::endl;
+using namespace std;
 
 int main()
 {
+
 	cout << "Task #1. Constant values" << endl;
 	double a = 2.1, b = 3.9, x = 0.1;
 	Calculator calculator(a, b);
 	cout << "sin(bx)/x * e^(-ax) = " << calculator(x) << endl;
 	cout << endl;
 	
+
 	cout << "Task #2. Using Calculator" << endl;
 	cout << "Type a, b, x: ";
 	cin >> a >> b >> x;
@@ -21,5 +21,4 @@ int main()
 	const auto result = user_calculator(x);
 	
 	cout << "sin(bx)/x * e^(-ax) = " << result << endl;
-	
 }
